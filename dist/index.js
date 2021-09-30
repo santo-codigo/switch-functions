fe84d06a1854e22612d71e31afc067442ba384ce
jeancigoli <jean@primi.com.br>
2021-09-30 09:43:53 -0300

    chore: version 1.0.0

--- dist/index.js
+++ dist/index.js
@@ -1,47 +1,14 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const switch_1 = require("./switch");
-const options = [
-    {
-        target: {
-            body: "sportClub",
-        },
-        expected: {
-            sportClub: 1,
-        },
-        handle: () => {
-            // Will run what club 1 needs
-        },
-    },
-    {
-        target: {
-            body: "sportClub",
-        },
-        expected: {
-            sportClub: 2,
-        },
-        handle: () => {
-            // Will run what club 2 needs
-        },
-    },
-    {
-        handle: () => {
-            // Here it will be executed if you do not enter in any case.
-        },
-    },
-];
-console.log((0, switch_1.adaptSwitchFunctions)(options));
-/*
-export type AdapterOptions<T = any> = {
-  target?: {
-    body?: string;
-    params?: string;
-    query?: string;
-    headers?: string;
-    [key: string]: string | undefined;
-  };
-  expected?: { [key: string]: any };
-  handle: Function;
-}[];
-*/
+__exportStar(require("./switch"), exports);
 //# sourceMappingURL=index.js.map
